badd79aa152d64247a6143e8ca012dd7e89b878d
Krzysiek <biakrz@gmail.com>
2011-05-02 23:59:42 +0200

    tworzenie tabeli uzytkownikow

---
+++ install.php
@@ -0,0 +1,27 @@
+<?
+
+require 'conn.php';
+
+ 
+
+$sql0="create database if not exists test";
+
+mysql_query($sql0) or die(mysql_error());
+
+$sql1="create table if not exists user(
+
+                            id int(11) NOT NULL auto_increment,
+
+                            login varchar(50) NOT NULL,
+
+                            password varchar (40) NOT NULL,
+
+                   
+
+         PRIMARY KEY (id))";
+
+mysql_query($sql1) or die(mysql_error());
+
+echo('Baza danych utworzona poprawnie')
+
+?>
