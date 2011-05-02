8733e78d385b86c3037e6b9414d8d90f70598c37
Krzysiek <biakrz@gmail.com>
2011-05-03 00:26:06 +0200

    zmiana danych bazy

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?
-define('DB_HOST','localhost');
-define('DB_USER','root'); 
-define('DB_PASS','aa'); 
-define('DB_DB','test');
+define('DB_HOST','mysql2.yoyo.pl');
+define('DB_USER','db1178590'); 
+define('DB_PASS','flash0'); 
+define('DB_DB','db1178590');
 
 $connect = mysql_connect(DB_HOST, DB_USER, DB_PASS)
 or die('Nie uda³o po³¹czyc siê z baz¹ danych. '.mysql_error());
