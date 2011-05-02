e657bcb86844664b77b4a667a766e22ff6e71987
Krzysiek <biakrz@gmail.com>
2011-05-03 00:24:57 +0200

    zmieniony uklad strony

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset="iso-8859-2" />
+<title>Polski JOPS</title>
+</head>
+<body LEFTMARGIN=150 TOPMARGIN=50 MARGINWIDTH=200 MARGINHEIGHT=100>
 <?php
 
 session_start();
@@ -10,14 +16,28 @@ echo "Jestes zalogowany jako: ";
 
 echo $_SESSION['login'];
 
+echo "<br>";
+
+echo ("<a href=\"logout.php\">Wyloguj</a>");
+
 }
 
 else
 
 {
 
-echo "Nie jestes zalogowany";
-
+echo "Nie jestes zalogowany"; 
  
+echo "<br>";
+
+echo ("<a href=\"login.php\">Zaloguj</a>");
+
+echo "<br>";
+
+echo ("<a href=\"register.php\">Rejestracja</a>");
 
 }
+?>
+
+</body>
+</html>
